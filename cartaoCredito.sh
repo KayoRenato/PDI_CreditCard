545f6d2d07773298a007d4bb7781e68462859196
Kayo Renato <kayorenatocontato@gmail.com>
2021-04-04 21:24:22 -0300

    update

---
+++ cartaoCredito.sh
@@ -0,0 +1,1 @@
+~/Documents/PDI/data-integration/kitchen.sh /file:"~/Documents/PDI/Projetos/CartaoCredito/cartaoCreditoGov.kjb" /level:Detail /logfile:"~/Documents/PDI/Projetos/CartaoCredito/cartaoCredito.log"
